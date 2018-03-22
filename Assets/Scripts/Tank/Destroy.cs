683691aa560c945a14b23c795d15bd3fd31c86b0
saboudreault <saboudreault@gmail.com>
2018-03-22 14:02:43 -0400

    * desert + tweaked audio, music should arrive later + tank is now destroyed at map's edge
    ? i haven't fixed the bug where if you hit hard enough an object at the wrong angle, the tank keeps rotating. it's somehow rare, and i feel that this would be a good amount of time to find it.
    
    * gallery
    - removed apartment, school and office scene. only gallery remains
    + added title, lights, post-effects
    
    ? this scene feels way more concise and coherent since i removed the extra other rooms. i think the point is good enough this way.
    ? the AI sucks. hard. but again, i don't want to waste time on this and it's actually interesting to shoot a passive AI.

---
+++ Assets/Scripts/Tank/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject tank;
+
+	private void OnTriggerExit(Collider other){
+		Destroy (tank);
+		Debug.Log ("BOY BYE");
+	}
+}
